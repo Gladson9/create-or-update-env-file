8de4391562676559b28f92f1bf3c532c84f0d601
Gladson9 <gladsondsouza77@gmail.com>
2024-03-04 23:58:05 +0530

    Fixed dist folder contents

--- dist/helpers/generateFilePath.js
+++
@@ -1,8 +0,0 @@
-import path from 'path';
-export default ({ baseDirectoryPath, targetDirectoryPath, fileName, }) => {
-    if (targetDirectoryPath.startsWith('./')) {
-        return path.join(baseDirectoryPath, targetDirectoryPath.slice(2), fileName);
-    }
-    return path.join(baseDirectoryPath, targetDirectoryPath, fileName);
-};
-//# sourceMappingURL=generateFilePath.js.map
